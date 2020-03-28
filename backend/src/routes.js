74d9d345c7a4d6e3267aed2068ee130afbe798d7
Hamad Ali <hamadmusaali@gmail.com>
2020-03-28 15:18:22 -0300

    Back-end da aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,16 +16,31 @@ routes.post('/ongs', celebrate({
     [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(11),
+        whatsapp: Joi.string().required().min(10).max(11),
         city: Joi.string().required(),
-        uf: Joi.string().required().length(2)
+        uf: Joi.string().required().length(2),
     })
 }), ongController.create);
 
-routes.get('/profile', profileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), profileController.index);
 
-routes.get('/incidents', incidentsController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    }) 
+}), incidentsController.index);
+
+
 routes.post('/incidents', incidentsController.create);
-routes.delete('/incidents/:id', incidentsController.delete);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number(),
+    })
+}), incidentsController.delete);
 
 module.exports = routes;
